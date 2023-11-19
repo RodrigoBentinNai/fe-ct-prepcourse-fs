d05a05b06f6c2db87a4e896baecaeb2ddfa27a6a
RODRIGO MARIO BENTIN NAI <rodrigo.bentin.rbn@gmail.com>
2023-11-19 11:40:32 -0300

    practicando1

---
+++ practicando.js
@@ -0,0 +1,71 @@
+/* ########## MODULO 5 - ARRAYS ########## */
+
+// var listaDeCompras = []
+
+// listaDeCompras[3] = 'Tomates';
+// listaDeCompras[1] = 'Lechuga';
+
+// var elementoDelArray = listaDeCompras[3];
+// console.log(elementoDelArray);
+
+// var nombres = ['Matias', 'Maria', 'Diego', 'Rosa'].length;
+// console.log(nombres);
+
+// // PUSH y UNSHIFT
+// var colores = ['Amarillo', 'Azul'];
+// colores.push('Rojo');
+// colores.unshift('Verde');
+
+// console.log(colores);
+// // POP y SHIFT
+// colores.shift();
+// colores.pop();
+
+// console.log(colores);
+// // INCLUDES
+// var pintores = ['Picasso', 'Velázquez', 'Van Gogh', 'Dalí'];
+// var existeDali = pintores.includes('Dalí');
+
+// console.log(existeDali);
+// // EVERY
+// var numeros = [ 1, 6, 8, 9, 43 ];
+// var cumplenCondicion = numeros.every( ( num ) => { num > 5 } );
+
+// console.log(cumplenCondicion);
+// // SPLIT
+// var palabra = 'Henri';
+// var palabraSeparada = palabra.split('');
+
+// console.log(palabraSeparada);
+// // JOIN
+// palabraSeparada.pop();
+// palabraSeparada.push("y");
+// var palabraArreglada = palabraSeparada.join('');
+
+// console.log(palabraArreglada);
+// // FOREACH
+// var numeros = [ 1, 2, 3, 4 ];
+// numeros.forEach( (num) => { console.log(num) } )
+// // MAP
+// var numeros = [ 1, 2, 3, 4 ];
+// var masUno = numeros.map( (num) => { return num + 1 } );
+
+// console.log(masUno);
+
+// // FOR
+// var arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr.length; i++) {
+//    console.log(arr[i]);
+// }
+// // WHILE
+// var arr = [];
+// while (arr.length < 5) {
+//    arr.push('Camilo');
+// }
+// console.log(arr);
+
+
+
+
+/* ########## MODULO 6 - OBJETOS ########## */
+
