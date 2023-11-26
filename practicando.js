d2110a93aae6b282de52463413aac6622ab42c60
RODRIGO MARIO BENTIN NAI <rodrigo.bentin.rbn@gmail.com>
2023-11-26 15:55:29 -0300

    M06 OK

--- practicando.js
+++ practicando.js
@@ -1,4 +1,6 @@
-/* ########## MODULO 5 - ARRAYS ########## */
+/*#########################################*/
+/*########### MODULO 5 - ARRAYS ###########*/
+/*#########################################*/
 
 // var listaDeCompras = []
 // listaDeCompras[3] = 'Tomates';
@@ -118,5 +120,92 @@
 // console.log(x);
 
 
-/* ########## MODULO 6 - OBJETOS ########## */
 
+/*##########################################*/
+/*########### MODULO 6 - OBJETOS ###########*/
+/*##########################################*/
+
+//Las claves son únicas en un objeto, solo puede haber una clave de ese nombre, aunque, varias claves pueden tener el mismo valor. Los valores pueden ser cualquier tipo de dato de Javascript; cadena, número, booleano, matriz, función o incluso otro objeto
+
+//var deportes = {
+//    conBalon: ['Futbol', 'Basketball', 'Golf'],
+//    sinBalon: ['Boxeo', 'Surf', 'Trekking'],
+//};
+
+//var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+
+
+
+/*1 - Para acceder a la propiedad de un objeto simplemente tenemos que escribir el nombre del objeto seguido de un punto y el nombre de la propiedad.
+
+2 - Para cambiar el valor de una propiedad simplemente tenemos que acceder a ella e igualarla al nuevo valor.
+
+3 - Para eliminar propiedades utilizaremos una palabra reservada llamada delete.*/
+
+// →→→→→ ACCEDER
+//var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+//console.log(persona.edad);
+// →→→→→ ASIGNAR
+//var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+//persona.nombre = 'Martín';
+//console.log(persona.nombre);
+// →→→→→ CREAR
+//var autos = {};
+//autos.marcas = ['Ford', 'Audi', 'Ferrari'];
+//console.log(autos);
+// →→→→→ BORRAR
+//delete autos.marcas;
+//console.log(autos);
+
+
+
+/*De la misma manera que utilizamos la Dot-Notation o notación por puntos para acceder o asignar un valor, también podemos usar Bracket-Notation, o notación por corchetes. Lo único que cambia es la forma en la que lo escribimos.
+
+Muchas veces nos puede suceder que necesitemos utilizar una variable externa para guardarla como propiedad en un objeto. Es importante que en esos casos recordemos utilizar Bracket-Notation sin comillas para que funcione correctamente.*/
+
+// →→→→→ DOT NOTATION
+//var atuendos = { manos: ['Guantes', 'Anillos'], pies: ['Zapatos', 'Soquetes'] };
+// →→→→→ BRACKET NOTATION
+//atuendos['piernas'] = ['Bermudas', 'Pantalones'];
+//console.log(atuendos);
+
+
+
+/*El método hasOwnProperty() nos permitirá especificar un nombre, y verificar si este existe como una propiedad dentro de un objeto. En cada caso devolverá true o false.
+
+El método Object.keys() devuelve todas las propiedades de un objeto guardadas en orden dentro de un arreglo.*/
+
+// →→→→→ HAS OWN PROPERTY
+//var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
+//var tienePropiedad = libro.hasOwnProperty('nombre');
+//console.log(tienePropiedad);
+
+// →→→→→ KEYS
+//var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
+//var todasLasPropiedades = Object.keys(libro);
+//console.log(todasLasPropiedades);*/
+
+//  →→→→→ FOR...IN
+//let mundo = { continentes: 7, paises: 195, oceanos: 5};
+//for (let prop in mundo) {
+//   console.log('Esta es la propiedad: ', prop);
+//   console.log('Este es el valor: ', mundo[prop]);
+//}
+
+// →→→→→ THIS
+//var mascota = {
+//   animal: 'Perro',
+//   raza: 'Ovejero Alemán',
+//   amistoso: true,
+//   dueño: 'María López',
+//   info: function () {
+//      console.log('Mi perro es un  ' + this.raza);
+//   },
+//};
+//mascota.info();
+
+
+
+/*##########################################*/
+/*########### MODULO 7 - ....... ###########*/
+/*##########################################*/
